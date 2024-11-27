8b24fd531d9d476e9a227fa0d1a4cba3ce4e291c
gabspaludo <gpf.gabriel.gpf@gmail.com>
2024-11-26 22:24:05 -0300

    commit 4

--- src/models/Movie.java
+++ src/models/Movie.java
@@ -1,3 +1,4 @@
+package models;
 public class Movie {
 
     public static final int  CHILDRENS = 2;
@@ -23,4 +24,5 @@ public class Movie {
     public String getTitle (){
         return _title;
     }
-  }
+    
+}
